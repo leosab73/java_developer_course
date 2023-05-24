25d960dddd66c507bb0896a73a8f6f50df9f417f
leosab73 <132715620+leosab73@users.noreply.github.com>
2023-05-24 20:10:08 +0200

    2.6

---
+++ lezione_7/e7_negozio/src/NegozioMain.java
@@ -0,0 +1,5 @@
+public class NegozioMain {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ lezione_7/e7_negozio/src/Prodotto.java
@@ -0,0 +1,13 @@
+public class Prodotto {
+    protected String nome;
+    protected double prezzo;
+    protected String genere;
+
+    public Prodotto(String nome, double prezzo, String genere) {
+        this.nome = nome;
+        this.prezzo = prezzo;
+        this.genere = genere;
+    }
+
+    
+}
