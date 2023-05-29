b9397a1542bfd4abe05046f2c54789a3eb4628a3
leosab73 <132715620+leosab73@users.noreply.github.com>
2023-05-29 11:11:56 +0200

    First version

---
+++ lezione_11/e01_giocodelloca/src/Giocatore.java
@@ -0,0 +1,13 @@
+public class Giocatore {
+    private int[] posizione;
+    private int punteggio;
+
+    public Giocatore() {
+        posizione = new int[] {0, 0};
+        punteggio = 0;
+    }
+
+    public void tiraDadi() {
+
+    }
+}
