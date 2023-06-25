2f2368ec0ad600b8663d638463af619b4abcebf4
l-sabino <132715620+leosab73@users.noreply.github.com>
2023-06-25 23:35:32 +0200

    Inizializing project

--- lezione_23/SharedMobility/src/veicolo/Furgone.java
+++ lezione_23/SharedMobility/src/veicolo/Furgone.java
@@ -5,7 +5,7 @@ import enumerazioni.Patente;
 
 public class Furgone extends VeicoloMotorizzato {
 
-    public Furgone(String id, String targa, Carburante carburante, Patente patente) {
+    public Furgone(String id, String targa, Carburante carburante) {
         super(id, targa, carburante, -7, -13, Patente.B);
     }
 }
